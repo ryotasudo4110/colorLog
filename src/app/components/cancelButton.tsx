15f5719c70fd95a034f7458c22db9899da3d846b
須藤稜太 <ryotasudo0114@gmail.com>
2025-07-29 15:39:52 +0900

    bug fix

--- src/app/components/cancelButton.tsx
+++ src/app/components/cancelButton.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import Link from "next/link";
+
 export default function CancelButton() {
 
     return (
-            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 ml-5">
-                <a href="/"> キャンセル </a>
-            </button>
+        <Link href="/" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 ml-5">
+            キャンセル
+        </Link>
     );
 }
--- src/app/components/homeButton.tsx
+++ src/app/components/homeButton.tsx
@@ -1,12 +1,14 @@
 'use client';
 
+import Link from "next/link";
+
 export default function HomeButton() {
 
     return (
         <div className="flex justify-center mt-6 mb-5">
-            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                <a href="/"> ホーム </a>
-            </button>
+            <Link href="/" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                ホーム
+            </Link>
         </div>
     );
 }
